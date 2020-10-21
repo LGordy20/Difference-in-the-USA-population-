833be96124290b8f26995b2e0a486d1babb42fd7
LGordy20 <liam.gordy@patelhs.org>
2020-10-21 02:39:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int oldPopulation = 282000000;
+    int currentPopulation = 331002651;
+    int increasePopulation = 49002651;
+    //USA old population is from 2000 while current is the population in 2020
+    if( increasePopulation <= 49002651);
+    {
+      System.out.print("The population in the USA form 2000 to 2020 is 49 " + increasePopulation);
+    } else { 
+      System.out.print("There was no change in population from 2000 to 2020.");
+    }
+  }
+}
